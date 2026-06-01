5d095377f95583ee802550ae1ab179d0f1aa61d1
Jenkins CI <jenkins@ci>
2026-06-01 14:51:49 +0000

    Deploy reports: srvtest04/concept/smoke [ci skip]

--- srvtest04/concept/smoke/jmeter/content/js/dashboard.js
+++ srvtest04/concept/smoke/jmeter/content/js/dashboard.js
@@ -173,7 +173,7 @@ $(document).ready(function() {
     });
 
     // Creates APDEX table
-    createTable($("#apdexTable"), {"supportsControllersDiscrimination": true, "overall": {"data": [0.9545454545454546, 500, 1500, "Total"], "isController": false}, "titles": ["Apdex", "T (Toleration threshold)", "F (Frustration threshold)", "Label"], "items": [{"data": [0.9545454545454546, 500, 1500, "GET /reserve.php"], "isController": false}]}, function(index, item){
+    createTable($("#apdexTable"), {"supportsControllersDiscrimination": true, "overall": {"data": [0.9090909090909091, 500, 1500, "Total"], "isController": false}, "titles": ["Apdex", "T (Toleration threshold)", "F (Frustration threshold)", "Label"], "items": [{"data": [0.9090909090909091, 500, 1500, "GET /reserve.php"], "isController": false}]}, function(index, item){
         switch(index){
             case 0:
                 item = item.toFixed(3);
@@ -187,7 +187,7 @@ $(document).ready(function() {
     }, [[0, 0]], 3);
 
     // Create statistics table
-    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 11, 0, 0.0, 392.27272727272725, 261, 1287, 288.0, 1118.6000000000006, 1287.0, 1287.0, 0.9627166112375284, 6.813801171669875, 0.1193994234640294], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["GET /reserve.php", 11, 0, 0.0, 392.27272727272725, 261, 1287, 288.0, 1118.6000000000006, 1287.0, 1287.0, 0.9627166112375284, 6.813801171669875, 0.1193994234640294], "isController": false}]}, function(index, item){
+    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 11, 0, 0.0, 376.54545454545456, 284, 868, 303.0, 799.2000000000003, 868.0, 868.0, 0.9468881811138848, 6.702445096625635, 0.11743632714986657], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["GET /reserve.php", 11, 0, 0.0, 376.54545454545456, 284, 868, 303.0, 799.2000000000003, 868.0, 868.0, 0.9468881811138848, 6.702445096625635, 0.11743632714986657], "isController": false}]}, function(index, item){
         switch(index){
             // Errors pct
             case 3:
